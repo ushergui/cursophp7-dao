c1b6550642a0bdfea9f792104260952e40547596
ushergui <guilherme-02-08@hotmail.com>
2018-12-30 12:29:36 -0200

    Criei a classe usuário

--- index.php
+++ index.php
@@ -1,12 +1,22 @@
 <?php
 require_once("config.php");
 
-$sql = new Sql();
+$root = new Usuario();
+
+$root->loadById(5);
+
+echo $root;
+
+
+
+
+
+/*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
 
 echo json_encode($usuarios);
+*/
 
 
-
-?>
+?> 
